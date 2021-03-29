991cc88d9a42047b2843803b4eb6bea609aad33a
icephat <phattaraporn.sa@ku.th>
2021-03-29 19:35:49 +0700

    คิดออกแล้วค่ะ เพิ่มเติมโค้ด

--- 6320500603_1.c
+++ 6320500603_1.c
@@ -1,10 +1,24 @@
 #include<stdio.h>
 int main()
 {
-    int n,c,i;
+    int n,c,i,a,sum=0,s,k=0,b;
     scanf("%d %d",&n,&c);
 
-
+    if(n>0&&n<32750)
+    {
+        a=n%10;
+        if(a<5)
+        {
+            n=n-a;
+        }
+        else if(a>4)
+        {
+            b=10-a;
+            n=n+b;
+        }
+        k+=n+c;
+        printf("%d",k);
+    }
 
 
 
