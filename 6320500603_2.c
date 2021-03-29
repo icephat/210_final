82bf2b1d43e695969c80782fde8108e9aaff76e1
icephat <phattaraporn.sa@ku.th>
2021-03-29 17:18:11 +0700

    วนลูปหาค่ามากที่สุด

--- 6320500603_2.c
+++ 6320500603_2.c
@@ -4,8 +4,19 @@ int main()
     int n;
     scanf("%d",&n);
 
-
-
+    int a[n],i,min;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    int max=a[0];
+    for(i=1;i<n;i++)
+    {
+        if(max<a[i])
+        {
+            max=a[i];
+        }
+    }
 
 
     return 0;
